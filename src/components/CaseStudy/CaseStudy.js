bb978f612043c8a67220e446db6bd29124f3a30a
Jeremiah Quill <jcq5010@gmail.com>
2022-05-10 20:50:33 -0400

    first draft of case study template

--- src/components/CaseStudy/CaseStudy.js
+++ src/components/CaseStudy/CaseStudy.js
@@ -11,35 +11,103 @@ const CaseStudy = () => {
           <button className="bg-black text-white py-2 px-5 rounded">Repo</button>
         </div>
       </div>
-      <div className="p-5 max-w-screen-lg m-auto">
-        <div className="rounded">
+      <div className="p-5 max-w-screen-md m-auto flex flex-col gap-20 my-20">
+        <div>
           <img className="rounded" src="./images/flow-with-megmo.png" />
         </div>
-        <h3 className="text-center">Summary</h3>
-        <p>
-          Ex occaecat reprehenderit laboris magna amet culpa aliqua consequat do aliqua ex.
-          Voluptate sunt proident nulla adipisicing sit elit enim. Do sunt eu aute anim elit
-          occaecat. Deserunt velit et mollit minim ullamco tempor aliquip mollit qui. dolor. Mollit
-          anim fugiat officia occaecat cupidatat fugiat culpa cillum Lorem culpa irure proident
-          laborum. Sint eu dolore culpa id proident qui elit culpa.
-        </p>
-        <h3>Technologies</h3>
-        <ul>
-          <li>react</li>
-          <li>tailwind css</li>
-          <li>react</li>
-          <li>tailwind css</li>
-          <li>react</li>
-          <li>tailwind css</li>
-          <li>react</li>
-          <li>tailwind css</li>
-          <li>react</li>
-          <li>tailwind css</li>
-          <li>react</li>
-          <li>tailwind css</li>
-          <li>react</li>
-          <li>tailwind css</li>
-        </ul>
+        <div>
+          <h3 className="text-center m-5">Summary</h3>
+          <p>
+            Ex occaecat reprehenderit laboris magna amet culpa aliqua consequat do aliqua ex.
+            Voluptate sunt proident nulla adipisicing sit elit enim. Do sunt eu aute anim elit
+            occaecat. Deserunt velit et mollit minim ullamco tempor aliquip mollit qui. dolor.
+            Mollit anim fugiat officia occaecat cupidatat fugiat culpa cillum Lorem culpa irure
+            proident laborum. Sint eu dolore culpa id proident qui elit culpa.
+          </p>
+        </div>
+        <div>
+          <h3 className="text-center m-5">Technologies</h3>
+          <ul className="flex w-full flex-wrap gap-5">
+            <li>react</li>
+            <li>tailwind css</li>
+            <li>react</li>
+            <li>tailwind css</li>
+            <li>react</li>
+            <li>tailwind css</li>
+            <li>react</li>
+            <li>tailwind css</li>
+            <li>react</li>
+            <li>tailwind css</li>
+            <li>react</li>
+            <li>tailwind css</li>
+            <li>react</li>
+            <li>tailwind css</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-center m-5">Approach</h3>
+          <p>
+            Aliqua ad sint veniam magna ipsum nulla ad commodo et pariatur pariatur qui duis ut.
+            Esse officia nisi ex reprehenderit. Amet elit esse incididunt dolor irure occaecat
+            incididunt aute. Anim labore reprehenderit exercitation qui dolore labore aliquip
+            cupidatat nulla ipsum. Dolor quis aute do officia in nulla proident labore occaecat
+            excepteur anim culpa officia enim. Excepteur velit Lorem pariatur est excepteur nisi
+            officia adipisicing amet.
+            <br></br>
+            <br></br>
+            Reprehenderit amet officia laboris adipisicing occaecat sint voluptate do ut esse do.
+            Excepteur velit do ipsum veniam cillum consequat. Ullamco eu do irure pariatur.
+            <br></br>
+            <br></br>
+            Ipsum fugiat duis labore Lorem reprehenderit qui laboris amet sit qui voluptate. Ea
+            laboris adipisicing elit qui enim sint elit. Consectetur non ad occaecat aliquip laborum
+            deserunt voluptate. Tempor sint irure dolor voluptate esse id quis do exercitation.
+            Veniam proident sit exercitation dolore. Proident voluptate eiusmod labore id cupidatat
+            exercitation qui.
+          </p>
+        </div>
+        <div>
+          <img className="rounded mb-5" src="./images/flow-with-megmo.png" />
+          <p>
+            Dolor excepteur ad voluptate commodo incididunt sit minim ad duis. Do incididunt officia
+            velit esse veniam minim ullamco ex duis mollit. Deserunt consectetur elit culpa officia
+            consequat laborum duis tempor nisi quis esse exercitation commodo.
+          </p>
+        </div>
+        <div>
+          <img className="rounded mb-5" src="./images/flow-with-megmo.png" />
+          <p>
+            Dolor excepteur ad voluptate commodo incididunt sit minim ad duis. Do incididunt officia
+            velit esse veniam minim ullamco ex duis mollit. Deserunt consectetur elit culpa officia
+            consequat laborum duis tempor nisi quis esse exercitation commodo.
+          </p>
+        </div>
+        <div>
+          <img className="rounded mb-5" src="./images/flow-with-megmo.png" />
+          <p>
+            Dolor excepteur ad voluptate commodo incididunt sit minim ad duis. Do incididunt officia
+            velit esse veniam minim ullamco ex duis mollit. Deserunt consectetur elit culpa officia
+            consequat laborum duis tempor nisi quis esse exercitation commodo.
+          </p>
+        </div>
+        <div>
+          <img className="rounded mb-5" src="./images/flow-with-megmo.png" />
+          <p>
+            Dolor excepteur ad voluptate commodo incididunt sit minim ad duis. Do incididunt officia
+            velit esse veniam minim ullamco ex duis mollit. Deserunt consectetur elit culpa officia
+            consequat laborum duis tempor nisi quis esse exercitation commodo.
+          </p>
+        </div>
+        <div>
+          <h3 className="text-center mb-5">Conclusions</h3>
+          <p>
+            Ex culpa anim anim est dolor nulla non. Lorem Lorem adipisicing fugiat pariatur amet
+            irure. Velit Lorem culpa labore ipsum ex id dolore non consequat culpa laboris quis.
+            Excepteur culpa aliqua sint nulla incididunt et aliquip labore id id velit aliquip.
+            Voluptate consequat eiusmod minim ipsum cupidatat commodo non ipsum. Proident dolor
+            nulla magna aliqua non excepteur aliquip irure pariatur.
+          </p>
+        </div>
       </div>
     </div>
   );
