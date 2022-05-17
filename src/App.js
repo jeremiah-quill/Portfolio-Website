9d5ff27c9f235bcda722efba758e284f28d97556
Jeremiah Quill <jcq5010@gmail.com>
2022-05-17 11:01:41 -0400

    added fade up on initial homepage load

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <AnimatePresence exitBeforeEnter initial={false}>
+      <AnimatePresence exitBeforeEnter initial={true}>
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Home />} />
           <Route path="/work" element={<Work />} />
