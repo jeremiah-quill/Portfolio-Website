0a4010350fb7b7067ab80c2cf72feb6cf4a23bb8
Jeremiah Quill <jcq5010@gmail.com>
2022-05-22 00:12:29 -0400

    fixed typo in first sentence of bio

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import projectData from '../content/content.js';
-import Project from '../components/Project';
-import Contact from '../components/Contact';
-import Socials from '../components/Socials';
-import { motion } from 'framer-motion';
-import { pageFade, pageFadeTransition } from '../animations/variants';
-import ProgressiveImg from '../components/ProgressiveImg';
+import React from "react";
+import projectData from "../content/content.js";
+import Project from "../components/Project";
+import Contact from "../components/Contact";
+import Socials from "../components/Socials";
+import { motion } from "framer-motion";
+import { pageFade, pageFadeTransition } from "../animations/variants";
+import ProgressiveImg from "../components/ProgressiveImg";
 
 const Home = () => {
   return (
@@ -26,11 +26,11 @@ const Home = () => {
             <ProgressiveImg
               className="bio-img"
               aspectRatio="1"
-              lowQualityImg={'./images/bio-img-compress_small.png'}
-              highQualityImg={'./images/bio-img-compress.png'}
+              lowQualityImg={"./images/bio-img-compress_small.png"}
+              highQualityImg={"./images/bio-img-compress.png"}
             />
             <p className="bio">
-              I'm a self-taught deveoper with a wide range of skills across the stack and a passion
+              I'm a self-taught developer with a wide range of skills across the stack and a passion
               for learning. I recently graduated from a full stack bootcamp offered by UPenn where I
               was able to refine my skills and experience working as part of a team.
               <br></br>
@@ -45,10 +45,10 @@ const Home = () => {
               passions. That's why I continue to grind every day to improve as a developer.
               <br></br>
               <br></br>
-              I'm currently available for{' '}
-              <span className="accent-orange-text">freelance opportunities</span>,{' '}
-              <span className="accent-orange-text">contract roles</span>, or{' '}
-              <span className="accent-orange-text">full time positions</span>. Please reach out @{' '}
+              I'm currently available for{" "}
+              <span className="accent-orange-text">freelance opportunities</span>,{" "}
+              <span className="accent-orange-text">contract roles</span>, or{" "}
+              <span className="accent-orange-text">full time positions</span>. Please reach out @{" "}
               <span className="accent-orange-text">jcq5010@gmail.com</span>, I would love to hear
               from you!
             </p>
@@ -64,8 +64,8 @@ const Home = () => {
               then I will make sure to take the time and dive deep when necessary.
               <br></br>
               <br></br>
-              I'm currently teaching myself <span className="important-text">Gatsby</span>,{' '}
-              <span className="important-text">Figma</span>, and{' '}
+              I'm currently teaching myself <span className="important-text">Gatsby</span>,{" "}
+              <span className="important-text">Figma</span>, and{" "}
               <span className="important-text">Tailwind</span>.
             </p>
             <div>
