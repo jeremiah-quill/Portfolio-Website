c7c5febe521fe5fd4bd83700caa80e941f12f68f
Jeremiah Quill <jcq5010@gmail.com>
2022-05-31 10:07:51 -0400

    fully centered socials bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,15 @@
-import React, { useState, useRef, useEffect, useMemo } from 'react';
-import { useLocation } from 'react-router-dom';
-import CustomLink from './CustomLink';
-import { BiChevronRight } from 'react-icons/bi';
-import { useWidthContext } from '../contexts/WidthContext';
-import { AnimatePresence, motion } from 'framer-motion';
+import React, { useState, useRef, useEffect, useMemo } from "react";
+import { useLocation } from "react-router-dom";
+import CustomLink from "./CustomLink";
+import { BiChevronRight } from "react-icons/bi";
+import { useWidthContext } from "../contexts/WidthContext";
+import { motion } from "framer-motion";
 
 function Header() {
   const ref = useRef();
   const [isMenuOpen, setIsMenuOpen] = useState(null);
   const location = useLocation();
   const { width } = useWidthContext();
-
-  const menu = {
-    show: {
-      transition: {
-        staggerChildren: 1,
-      },
-    },
-  };
 
   const dropdownLink = {
     hide: {
@@ -34,14 +26,14 @@ function Header() {
 
   const memoizedPathname = useMemo(() => {
     switch (location.pathname) {
-      case '/flow-with-megmo':
-        return 'Flow with Megmo';
-      case '/wishlist':
-        return 'Wishlist';
-      case '/playlist-palette':
-        return 'Playlist Palette';
+      case "/flow-with-megmo":
+        return "Flow with Megmo";
+      case "/wishlist":
+        return "Wishlist";
+      case "/playlist-palette":
+        return "Playlist Palette";
       default:
-        return '';
+        return "";
     }
   }, [location]);
 
@@ -49,13 +41,15 @@ function Header() {
     function closeMenu(e) {
       if (e.target !== ref.current) setIsMenuOpen(false);
     }
-    document.addEventListener('click', closeMenu);
-    return () => document.removeEventListener('click', closeMenu);
+    document.addEventListener("click", closeMenu);
+    return () => document.removeEventListener("click", closeMenu);
   }, []);
 
   return (
-    <header className="z-50 flex items-center fixed bg-white w-full py-2 h-12 top-0">
-      <nav className="flex items-stretch">
+    <header className="z-50 flex justify-between items-center fixed bg-white w-full py-2 h-12 top-0">
+      {/* <div className="flex justify-between max-w-5xl px-5 m-auto"> */}
+      {/* <h1 className="text-black text-2xl">JQ</h1> */}
+      <nav className="flex items-stretch m-auto">
         <CustomLink to="/">Home</CustomLink>
         <div>
           <div className="relative z-10 bg-white flex items-center">
@@ -68,12 +62,12 @@ function Header() {
             </button>
             {width > 500 ? (
               <motion.div
-                initial={{ rotate: '90deg' }}
-                animate={memoizedPathname !== '' ? { rotate: '0deg' } : { rotate: '90deg' }}>
-                <BiChevronRight className="m-0 p-0" style={{ color: 'black' }} />
+                initial={{ rotate: "90deg" }}
+                animate={memoizedPathname !== "" ? { rotate: "0deg" } : { rotate: "90deg" }}>
+                <BiChevronRight className="m-0 p-0" style={{ color: "black" }} />
               </motion.div>
             ) : null}
-            {width > 500 && memoizedPathname !== '' ? (
+            {width > 500 && memoizedPathname !== "" ? (
               <div className="text-black h-full flex items-center absolute left-full top-0.5 -translate-y-0.5 -ml-2">
                 <motion.h3
                   initial={{ opacity: 0, x: -20 }}
@@ -99,6 +93,7 @@ function Header() {
           ) : null}
         </div>
       </nav>
+      {/* </div> */}
     </header>
   );
 }
